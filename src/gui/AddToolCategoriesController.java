4a5b5cda28e0f009bde8d98f7e31aad325b1a732
Ryan LaRue <28721348+Ryanmmlarue@users.noreply.github.com>
2020-10-20 04:57:31 -0400

    Adding a Tool now prints out all of the needed information to insert data into the database - needs to be tied into specific queries.

--- src/gui/AddToolCategoriesController.java
+++ src/gui/AddToolCategoriesController.java
@@ -1,17 +1,24 @@
 package gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author Ryan LaRue, rml5169@rit.edu
  */
 public class AddToolCategoriesController extends Controller{
+
 
     @FXML
     VBox categoryVBox;
@@ -25,25 +32,43 @@ public class AddToolCategoriesController extends Controller{
         List<String> categories = SQLController.getCategories();
         for (String category : categories) {
             RadioButton button = new RadioButton();
+            button.setPrefWidth(200);
             button.setText(category);
             categoryVBox.getChildren().add(button);
         }
     }
 
-
+    @FXML
     public void addNewCategory() {
         String category_name = newCategoryTextField.getText().toUpperCase();
-        boolean status = SQLController.insertCategory(category_name);
-        if (status) {
-            statusText.setVisible(false);
-            RadioButton button = new RadioButton();
-            button.setText(category_name);
-            categoryVBox.getChildren().add(button);
-        } else {
+        if (category_name.equals("")) {
             statusText.setVisible(true);
+        } else {
+            boolean status = SQLController.insertCategory(category_name);
+            if (status) {
+                statusText.setVisible(false);
+                RadioButton button = new RadioButton();
+                button.setPrefWidth(200);
+                button.setText(category_name);
+                categoryVBox.getChildren().add(button);
+            } else {
+                statusText.setVisible(true);
+            }
         }
     }
 
-
+    @FXML
+    public void submitCategories(ActionEvent event) {
+        List<String> categories = new ArrayList<>();
+        for (Node node : categoryVBox.getChildren()) {
+            if (node instanceof RadioButton) {
+                if(((RadioButton)node).isSelected()) {
+                    categories.add(((RadioButton)node).getText());
+                }
+            }
+        }
+        AddToolController.setCategoriesList(categories);
+        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+    }
 
 }
--- src/gui/AddToolController.java
+++ src/gui/AddToolController.java
@@ -7,18 +7,33 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * @author Ryan LaRue, rml5169@rit.edu
  */
 public class AddToolController extends Controller{
     //TODO: Populate Tool Category ChoiceBox with categories from database
 
+    private static List<String> categories;
+
+    @FXML
+    RadioButton yesPurchasableRadioButton;
     @FXML
     RadioButton yesLendableRadioButton;
     @FXML
     TextField priceTextField;
     @FXML
     TextField toolNameTextField;
+
+    public static void setCategoriesList(List<String> categoriesList) {
+        categories = categoriesList;
+    }
+
 
     @FXML
     public void addCategories() {
@@ -27,7 +42,24 @@ public class AddToolController extends Controller{
 
     @FXML
     public void addTool(ActionEvent event){
-        //TODO: Add Tool to Database
+
+        System.out.println("Tool Name: " + toolNameTextField.getText());
+        System.out.println("Sale Price: " + priceTextField.getText());
+        System.out.println("Lendable: " + yesLendableRadioButton.isSelected());
+        System.out.println("Purchasable: " + yesPurchasableRadioButton.isSelected());
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+        System.out.println("Purchase Date:" + dateFormat.format(date));
+
+        //TODO: Create Insert Query into various needed tables
+        //TODO: Figure out how to find auto incremented TID
+
+        System.out.print("Categories: ");
+        for (String s: categories) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
         gotoHome(event);
     }
 }
