68d5267f5057af8c11a1264b7e15e1e6cfc1090b
Aditya Khanna <ak3064@rit.edu>
2020-10-23 18:06:40 -0400

    Commented Credentials

--- src/gui/Credentials.java
+++ src/gui/Credentials.java
@@ -6,14 +6,26 @@ public class Credentials {
     private static String username = "p320_19";
     private static String password = "shagiPh8vier5ooxeing";
 
+    /**
+     * Returns the server url
+      * @return url to the server
+     */
     public static String getUrl() {
         return url;
     }
 
+    /**
+     * Returns the username for postgres
+     * @return username for postgres
+     */
     public static String getUsername() {
         return username;
     }
 
+    /**
+     * Returns the password for postgres
+     * @return password for postgres
+     */
     public static String getPassword() {
         return password;
     }
