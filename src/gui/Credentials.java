b26cde1b6cbe6ec5926f6049fe9cf4660a783f08
Aditya Khanna <ak3064@rit.edu>
2020-10-23 17:56:23 -0400

    Commented code for OwnedTool and OwnedUserTools and then added the Credentials class in case of a direct clone

---
+++ src/gui/Credentials.java
@@ -0,0 +1,20 @@
+package gui;
+
+public class Credentials {
+
+    private static String url = "jdbc:postgresql://reddwarf.cs.rit.edu:5432/p320_19";
+    private static String username = "p320_19";
+    private static String password = "shagiPh8vier5ooxeing";
+
+    public static String getUrl() {
+        return url;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+}
