958eadf4036fd718df534da9a051215316c9505f
Daniel Castellarin <drc3203@rit.edu>
2020-11-14 14:34:50 -0500

    Creating a user class so each user can have their own preferences.

---
+++ src/data/User.java
@@ -0,0 +1,10 @@
+package data;
+
+public class User {
+
+    // likelyhood to sell
+    // likelyhood to lend
+    // likelyhood to changeprice
+    // likelyhood to return tool
+    // category preference
+}
